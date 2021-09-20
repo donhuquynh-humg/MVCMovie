b0c59459109490fc1b39f6a236e1b5d93450a572
QUYNH DO NHU <1821050481@student.humg.edu.vn>
2021-09-20 21:41:58 +0700

    Add project files.

---
+++ MVCMovie/Data/MVCMovieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCMovie.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MVCMovie.Data
+{
+    public class MVCMovieContext : DbContext
+    {
+        public MVCMovieContext (DbContextOptions<MVCMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
